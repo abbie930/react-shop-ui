28c81fea438689b8f83d6a4b98ba3cab65b82df7
abbie930 <abbiecheng930@gmail.com>
2023-07-05 23:36:19 +0800

    feat: add slide itmes

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'https://media.istockphoto.com/id/1142399373/photo/girls-who-were-shopping.jpg?s=612x612&w=0&k=20&c=VMAW7o4JaTKCsDmXV1E3cExfWZl54q_GS1TirTkM4Fg=',
+    title: 'Discover the Best. Delivered to You.',
+    desc: '“We’ve got the right stuff for you!”',
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: 'https://media.istockphoto.com/id/1278236689/photo/beauty-portrait-of-young-asian-woman-on-the-white-background.jpg?s=612x612&w=0&k=20&c=kEalGGZHHFP7I0DBGewlGOduIbMS7b_ST9k4X9HZEmA=',
+    title: 'Beauty Collection',
+    desc: 'Love yourself.',
+    bg: 'fcf1ed',
+  },
+  {
+    id: 3,
+    img: 'https://media.istockphoto.com/id/1261698208/vector/coming-soon-ink-brush-vector-lettering-promotion-or-announcement-banner-modern-vector.jpg?s=612x612&w=0&k=20&c=nKUyh-A4a65lkwZDz7nb2aBXfBIY7-NinZJkjnItEQg=',
+    title: 'NEW ARRIVALS',
+    desc: 'Discover the Latest Must-Have Pieces.',
+    bg: 'fbf0f4',
+  },
+]
