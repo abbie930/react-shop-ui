4c3aebdc1a426ae922face478cd6fe008fc9311b
abbie930 <abbiecheng930@gmail.com>
2023-07-06 13:07:33 +0800

    feat: add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,128 @@
+import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from "@material-ui/icons"
+import styled from "styled-components"
+
+const Container = styled.div`
+  display: flex;
+`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`
+const Logo = styled.h1``
+const Desc = styled.p`
+  margin: 20px 0px;
+`
+const SocialContainer = styled.div`
+  // icons in the same row
+  display: flex;
+`
+const SocialIcon = styled.div`
+ width: 40px;
+ height: 40px;
+ border-radius: 50%;
+ color: white;
+ background-color: #${props => props.color};
+ // icons 置中在圓圈圈裡
+ display: flex;
+ align-items: center;
+ justify-content: center;
+ margin-right: 20px;
+`
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`
+const Title = styled.h3`
+  margin-bottom: 30px;
+`
+
+const List = styled.ul`
+  // ul has its own m & p, so deleted them
+  margin: 0;
+  padding: 0;
+  // delete original style (dots)
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`
+const Payment = styled.img`
+  width: 50%;
+`
+
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>YC.Selected</Logo>
+        <Desc>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore eos expedita nesciunt ut incidunt fuga recusandae ab laudantium vel quod. Porro sapiente ducimus architecto alias impedit id autem error dolorem?
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Info</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{marginRight:"10px"}} /> 1234 main street
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{marginRight:"10px"}} />
+          00-0000-0000
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{marginRight:"10px"}} />
+          contact@ycselected.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  )
+}
+
+export default Footer
