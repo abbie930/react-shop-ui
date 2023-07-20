d5f164c09984f135545652c0c547501e9c110c2a
abbie930 <abbiecheng930@gmail.com>
2023-07-20 19:44:13 +0800

    feat: change login layout and add creat an account btn

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,7 @@ import { mobile } from '../responsive'
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { login } from '../redux/apiCalls'
+import { Link } from 'react-router-dom'
 
 const Container = styled.div``
 
@@ -16,10 +17,10 @@ const LoginContainer = styled.div`
 `
 
 const Wrapper = styled.div`
-  width: 25%;
+  width: 20%;
   padding: 20px;
   background-color: white;
-  ${mobile({ width: "75%" })}
+  ${mobile({ width: '75%' })}
 `
 const Title = styled.h1`
   font-size: 24px;
@@ -41,14 +42,14 @@ const Form = styled.form`
 const Input = styled.input`
   flex: 1;
   margin: 20px 0px 0px 0px;
-  padding: 15px;
+  padding: 18px;
   border: 0.5px solid gray;
 `
 
 const Button = styled.button`
   /* width: 40%; */
   border: none;
-  margin: 60px 0px 20px 0;
+  margin: 20px 0px 20px 0;
   padding: 15px 20px;
   background-color: #f5e5cc;
   cursor: pointer;
@@ -61,28 +62,52 @@ const Button = styled.button`
   }
 `
 
-const Link = styled.a`
-  margin: 5px 0px;
-  font-size: 14px;
-  text-decoration: underline;
-  cursor: pointer;
-`
-
 const Error = styled.span`
   color: red;
   margin-bottom: 10px;
 `
 
+const RegisterButton = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 20px;
+`
+const Or = styled.span`
+  display: flex;
+  justify-content: center;
+`
+
+// Create a custom-styled link button using styled-components
+const StyledLinkButton = styled(Link)`
+  background-color: transparent;
+  border: 1px solid black;
+  cursor: pointer;
+  text-align: center;
+  padding: 15px 20px;
+  margin-top: 20px;
+  text-decoration: none;
+  color: black;
+`
+const ForgetLink = styled.a`
+  margin: 20px 0px;
+  font-size: 14px;
+  text-align: end;
+  color: gray;
+  text-decoration: underline;
+  cursor: pointer;
+`
 
 const Login = () => {
   const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('') 
+  const [password, setPassword] = useState('')
   const dispatch = useDispatch()
   const { isFetching, error } = useSelector((state) => state.user)
 
   const handleClick = (e) => {
     e.preventDefault()
-    login(dispatch, {username, password})
+    login(dispatch, { username, password })
   }
 
   return (
@@ -95,10 +120,15 @@ const Login = () => {
           <Form>
             <Input placeholder="username" onChange={(e) => setUsername(e.target.value)} />
             <Input placeholder="password" type="password" onChange={(e) => setPassword(e.target.value)} />
-            <Button onClick={handleClick} disabled={isFetching}>SIGN IN</Button>
+            <ForgetLink>Forgot password?</ForgetLink>
+            <Button onClick={handleClick} disabled={isFetching}>
+              SIGN IN
+            </Button>
             {error && <Error>Something went wrong...</Error>}
-            <Link>Forgot password?</Link>
-            <Link>Create a new account</Link>
+            <RegisterButton>
+              <Or>Don’t have an account?</Or>
+              <StyledLinkButton to="/register">Create a new account</StyledLinkButton>
+            </RegisterButton>
           </Form>
         </Wrapper>
       </LoginContainer>
