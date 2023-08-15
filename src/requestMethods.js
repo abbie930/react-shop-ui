0a0d3a3a783169736ce4a0bd3d0c5367d9f7edf5
abbie930 <abbiecheng930@gmail.com>
2023-08-15 15:27:09 +0800

    fix: TOKEN issue

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
 const BASE_URL = process.env.REACT_APP_BASE_URL
-const TOKEN = process.env.REACT_APP_TOKEN
+
+
+const user = JSON.parse(localStorage.getItem('persist:root'))?.user
+const currentUser = user && JSON.parse(user).currentUser
+const TOKEN = currentUser?.accessToken
+
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL
